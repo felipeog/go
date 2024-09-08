32bd657113c861644ad03d5d60d14850f874eee8
felipeog <17603069+felipeog@users.noreply.github.com>
2024-09-08 15:20:36 -0300

    add exercism 004

---
+++ practice/exercism/004-weather-forecast/weather_forecast.go
@@ -0,0 +1,15 @@
+// Package weather provides weather forecast tools.
+package weather
+
+// CurrentCondition represents a certain condition.
+var CurrentCondition string
+
+// CurrentLocation represents a certain location.
+var CurrentLocation string
+
+// Forecast returns a string value describing the weather forecast.
+func Forecast(city, condition string) string {
+	CurrentLocation, CurrentCondition = city, condition
+
+	return CurrentLocation + " - current weather condition: " + CurrentCondition
+}
