96b742aebd66f6fbfaf53548b00cfc652fdf580b
felipeog <17603069+felipeog@users.noreply.github.com>
2024-09-08 10:57:57 -0300

    format

--- practice/exercism/000-hello-world/hello_world_test.go
+++ practice/exercism/000-hello-world/hello_world_test.go
@@ -6,8 +6,10 @@ import "testing"
 // and returns a string.
 // In other words, define a function with the following signature:
 // HelloWorld() string
+
 func TestHelloWorld(t *testing.T) {
 	expected := "Hello, World!"
+
 	if observed := HelloWorld(); observed != expected {
 		t.Fatalf("HelloWorld() = %v, want %v", observed, expected)
 	}
@@ -33,6 +35,7 @@ func BenchmarkHelloWorld(b *testing.B) {
 	if testing.Short() {
 		b.Skip("skipping benchmark in short mode.")
 	}
+
 	for i := 0; i < b.N; i++ {
 		HelloWorld()
 	}
